3b5cfda58ec83914e3208834b956f3919efb36bb
Przemyslaw Markiewicz <pmarkiewicz@oanda.com>
2021-10-04 19:56:24 +0200

    created api_service app, added DRF

--- excel_api/settings.py
+++ excel_api/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api_service',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
