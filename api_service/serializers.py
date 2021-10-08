7e410beb3f4cd2fc8be84ef13c7d60af086e68ff
Przemyslaw Markiewicz <pmarkiewicz@oanda.com>
2021-10-08 23:31:48 +0200

    Added ListField To serializer

--- api_service/serializers.py
+++ api_service/serializers.py
@@ -6,3 +6,4 @@ class FileSerializer(serializers.Serializer):
     file = serializers.FileField(
         validators=[FileExtensionValidator(allowed_extensions=['xlsx', 'xls'])]
     )
+    columns = serializers.ListField(child=serializers.CharField())
--- api_service/views.py
+++ api_service/views.py
@@ -22,5 +22,9 @@ class FileUploadView(GenericAPIView):
         if serializer.is_valid(raise_exception=True):
             data = serializer.validated_data
             resume = data['file']
-            return Response({'file name': str(resume)})
+            columns = data['columns'][0].split(',')
+            return Response({
+                'file': str(resume),
+                'columns': columns
+            })
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
